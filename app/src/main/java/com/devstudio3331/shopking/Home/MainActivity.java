2d566b5677f071c995e18cef3a95743a25c4c1be
Hrishikesh Ugale <43084197+Hrishi-3331@users.noreply.github.com>
2020-12-30 21:54:19 +0530

    Home and Splash Screen Design

--- app/src/main/java/com/devstudio3331/shopking/Home/MainActivity.java
+++ app/src/main/java/com/devstudio3331/shopking/Home/MainActivity.java
@@ -1,16 +1,40 @@
 package com.devstudio3331.shopking.Home;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.graphics.drawable.DrawerArrowDrawable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 import com.devstudio3331.shopking.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawer;
+    private NavigationView mNavigation;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("");
+
+        mDrawer = (DrawerLayout) findViewById(R.id.mDrawer);
+        mNavigation = (NavigationView) findViewById(R.id.mNavigation);
+        mToggle = new ActionBarDrawerToggle(MainActivity.this, mDrawer, toolbar, R.string.open, R.string.close);
+        mDrawer.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 }
--- app/src/main/java/com/devstudio3331/shopking/Home/SplashActivity.java
+++ app/src/main/java/com/devstudio3331/shopking/Home/SplashActivity.java
@@ -2,7 +2,9 @@ package com.devstudio3331.shopking.Home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.devstudio3331.shopking.R;
 
@@ -12,5 +14,15 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1500);
+
     }
 }
