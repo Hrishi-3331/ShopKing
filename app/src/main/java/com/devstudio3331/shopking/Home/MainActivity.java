4fefecd8506ab84c54384cc0470d035e4ec13e64
Hrishikesh Ugale <43084197+Hrishi-3331@users.noreply.github.com>
2020-12-30 20:27:56 +0530

    Started blank project

---
+++ app/src/main/java/com/devstudio3331/shopking/Home/MainActivity.java
@@ -0,0 +1,16 @@
+package com.devstudio3331.shopking.Home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.devstudio3331.shopking.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/devstudio3331/shopking/Home/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.devstudio3331.shopking.Home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.devstudio3331.shopking.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
